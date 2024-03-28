c484c8dc46f8e6450eb6f69de70e0b6c21783dc6
aziz bilgili <abilgili35@gmail.com>
2024-03-28 02:42:09 -0700

    Add project files.

---
+++ CA_OgrenciIsleri/Concretes/OgrenciConcrete.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA_OgrenciIsleri.Concretes
+{
+    public class OgrenciConcrete : IOgrenci
+    {
+        private static List<Ogrenci> ogrenciler = new List<Ogrenci>();
+
+        public void AddOgrenci(Ogrenci ogrenci)
+        {
+            ogrenciler.Add(ogrenci);
+        }
+
+        public bool DeleteOgrenci(int id)
+        {
+            foreach(Ogrenci o in ogrenciler)
+            {
+                if (o.Id == id)
+                {
+                    ogrenciler.Remove(o);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public List<Ogrenci> GetOgrenciler()
+        {
+            return ogrenciler;
+        }
+
+        public bool UpdateOgrenci(Ogrenci updated)
+        {
+            foreach (Ogrenci o in ogrenciler)
+            {
+                if (o.Id == updated.Id)
+                {
+                    o.Ad = updated.Ad;
+                    o.Soyad = updated.Soyad;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        
+    }
+}
---
+++ CA_OgrenciIsleri/Interfaces/IOgrenci.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA_OgrenciIsleri
+{
+    public interface IOgrenci
+    {
+        void AddOgrenci(Ogrenci ogrenci);
+        List<Ogrenci> GetOgrenciler();
+        bool UpdateOgrenci(Ogrenci ogrenci);
+        bool DeleteOgrenci(int id);
+    }
+}
