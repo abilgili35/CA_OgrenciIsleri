d949ea5160c5f2fb7fac10cf3b87055913ac9578
aziz bilgili <abilgili35@gmail.com>
2024-03-28 02:58:23 -0700

    Proje Program.cs icerisinde test edildi.

--- CA_OgrenciIsleri/Program.cs
+++ CA_OgrenciIsleri/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CA_OgrenciIsleri.Concretes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,80 @@ namespace CA_OgrenciIsleri
     {
         static void Main(string[] args)
         {
+            OgrenciConcrete concrete = new OgrenciConcrete();
+
+            concrete.AddOgrenci(new Ogrenci()
+            {
+                Id = 1,
+                Ad = "Aziz",
+                Soyad = "Bilgili"
+            }
+            ) ;
+
+            concrete.AddOgrenci(new Ogrenci()
+            {
+                Id = 2,
+                Ad = "Fatih",
+                Soyad = "Gunalp"
+            }
+            );
+
+            concrete.AddOgrenci(new Ogrenci()
+            {
+                Id = 3,
+                Ad = "Mauro",
+                Soyad = "Icardi"
+            }
+            );
+
+            foreach (Ogrenci o in concrete.GetOgrenciler())
+            {
+                Console.WriteLine( o );
+            }
+
+            Ogrenci ogrenci = new Ogrenci()
+            {
+                Id = 1,
+                Ad = "Ali",
+                Soyad = "Bilgili"
+            };
+
+            bool isUpdated = concrete.UpdateOgrenci( ogrenci );
+
+            if( isUpdated )
+            {
+                Console.WriteLine("Guncelleme basarili oldu.");
+
+                foreach (Ogrenci o in concrete.GetOgrenciler())
+                {
+                    Console.WriteLine(o);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Guncelleme basarisiz oldu.");
+            }
+
+            bool isDeleted = concrete.DeleteOgrenci(2);
+
+            if (isDeleted)
+            {
+                Console.WriteLine("Silme islemi basarili oldu.");
+
+                foreach (Ogrenci o in concrete.GetOgrenciler())
+                {
+                    Console.WriteLine(o);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Silme islemi basarisiz oldu.");
+            }
+
+
+            Console.Read();
+
+
         }
     }
 }
